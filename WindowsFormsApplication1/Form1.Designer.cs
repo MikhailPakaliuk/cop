f071937ece1064e81df0c82d8b9d98ce88c26bde
Michail Dyurilen <mr.dyurilen@mail.ru>
2016-02-09 13:58:55 +0300

    start project

---
+++ WindowsFormsApplication1/Form1.Designer.cs
@@ -0,0 +1,241 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Требуется переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Обязательный метод для поддержки конструктора - не изменяйте
+        /// содержимое данного метода при помощи редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBoxLongBottomBase = new System.Windows.Forms.TextBox();
+            this.textBoxWidhtBottomBase = new System.Windows.Forms.TextBox();
+            this.textBoxLongUpperBase = new System.Windows.Forms.TextBox();
+            this.textBoxWidhtUpperBase = new System.Windows.Forms.TextBox();
+            this.textBoxOffsetLong = new System.Windows.Forms.TextBox();
+            this.textBoxOffsetWidth = new System.Windows.Forms.TextBox();
+            this.labelLongBottomBase = new System.Windows.Forms.Label();
+            this.labelWidhtBottomBase = new System.Windows.Forms.Label();
+            this.labelLongUpperBase = new System.Windows.Forms.Label();
+            this.labelWidhtUpperBase = new System.Windows.Forms.Label();
+            this.labelOffsetLong = new System.Windows.Forms.Label();
+            this.labelOffsetWidth = new System.Windows.Forms.Label();
+            this.labelHeight = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(306, 164);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(418, 164);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // textBoxLongBottomBase
+            // 
+            this.textBoxLongBottomBase.Location = new System.Drawing.Point(177, 28);
+            this.textBoxLongBottomBase.Name = "textBoxLongBottomBase";
+            this.textBoxLongBottomBase.Size = new System.Drawing.Size(65, 20);
+            this.textBoxLongBottomBase.TabIndex = 2;
+            this.textBoxLongBottomBase.TextChanged += new System.EventHandler(this.textBoxLongBase_TextChanged);
+            // 
+            // textBoxWidhtBottomBase
+            // 
+            this.textBoxWidhtBottomBase.Location = new System.Drawing.Point(428, 28);
+            this.textBoxWidhtBottomBase.Name = "textBoxWidhtBottomBase";
+            this.textBoxWidhtBottomBase.Size = new System.Drawing.Size(65, 20);
+            this.textBoxWidhtBottomBase.TabIndex = 3;
+            // 
+            // textBoxLongUpperBase
+            // 
+            this.textBoxLongUpperBase.Location = new System.Drawing.Point(177, 71);
+            this.textBoxLongUpperBase.Name = "textBoxLongUpperBase";
+            this.textBoxLongUpperBase.Size = new System.Drawing.Size(65, 20);
+            this.textBoxLongUpperBase.TabIndex = 4;
+            // 
+            // textBoxWidhtUpperBase
+            // 
+            this.textBoxWidhtUpperBase.Location = new System.Drawing.Point(428, 71);
+            this.textBoxWidhtUpperBase.Name = "textBoxWidhtUpperBase";
+            this.textBoxWidhtUpperBase.Size = new System.Drawing.Size(65, 20);
+            this.textBoxWidhtUpperBase.TabIndex = 5;
+            // 
+            // textBoxOffsetLong
+            // 
+            this.textBoxOffsetLong.Location = new System.Drawing.Point(177, 117);
+            this.textBoxOffsetLong.Name = "textBoxOffsetLong";
+            this.textBoxOffsetLong.Size = new System.Drawing.Size(65, 20);
+            this.textBoxOffsetLong.TabIndex = 6;
+            // 
+            // textBoxOffsetWidth
+            // 
+            this.textBoxOffsetWidth.Location = new System.Drawing.Point(428, 117);
+            this.textBoxOffsetWidth.Name = "textBoxOffsetWidth";
+            this.textBoxOffsetWidth.Size = new System.Drawing.Size(65, 20);
+            this.textBoxOffsetWidth.TabIndex = 7;
+            // 
+            // labelLongBottomBase
+            // 
+            this.labelLongBottomBase.AutoSize = true;
+            this.labelLongBottomBase.Location = new System.Drawing.Point(22, 31);
+            this.labelLongBottomBase.Name = "labelLongBottomBase";
+            this.labelLongBottomBase.Size = new System.Drawing.Size(149, 13);
+            this.labelLongBottomBase.TabIndex = 8;
+            this.labelLongBottomBase.Text = "Длинна нижнего основания";
+            // 
+            // labelWidhtBottomBase
+            // 
+            this.labelWidhtBottomBase.AutoSize = true;
+            this.labelWidhtBottomBase.Location = new System.Drawing.Point(273, 31);
+            this.labelWidhtBottomBase.Name = "labelWidhtBottomBase";
+            this.labelWidhtBottomBase.Size = new System.Drawing.Size(149, 13);
+            this.labelWidhtBottomBase.TabIndex = 9;
+            this.labelWidhtBottomBase.Text = "Ширина нижнего основания";
+            // 
+            // labelLongUpperBase
+            // 
+            this.labelLongUpperBase.AutoSize = true;
+            this.labelLongUpperBase.Location = new System.Drawing.Point(19, 74);
+            this.labelLongUpperBase.Name = "labelLongUpperBase";
+            this.labelLongUpperBase.Size = new System.Drawing.Size(152, 13);
+            this.labelLongUpperBase.TabIndex = 10;
+            this.labelLongUpperBase.Text = "Длинна верхнего основания";
+            // 
+            // labelWidhtUpperBase
+            // 
+            this.labelWidhtUpperBase.AutoSize = true;
+            this.labelWidhtUpperBase.Location = new System.Drawing.Point(273, 74);
+            this.labelWidhtUpperBase.Name = "labelWidhtUpperBase";
+            this.labelWidhtUpperBase.Size = new System.Drawing.Size(147, 13);
+            this.labelWidhtUpperBase.TabIndex = 11;
+            this.labelWidhtUpperBase.Text = "Ширина вернего основания";
+            // 
+            // labelOffsetLong
+            // 
+            this.labelOffsetLong.AutoSize = true;
+            this.labelOffsetLong.Location = new System.Drawing.Point(56, 120);
+            this.labelOffsetLong.Name = "labelOffsetLong";
+            this.labelOffsetLong.Size = new System.Drawing.Size(115, 13);
+            this.labelOffsetLong.TabIndex = 12;
+            this.labelOffsetLong.Text = "Смещение по длинне";
+            // 
+            // labelOffsetWidth
+            // 
+            this.labelOffsetWidth.AutoSize = true;
+            this.labelOffsetWidth.Location = new System.Drawing.Point(303, 120);
+            this.labelOffsetWidth.Name = "labelOffsetWidth";
+            this.labelOffsetWidth.Size = new System.Drawing.Size(117, 13);
+            this.labelOffsetWidth.TabIndex = 13;
+            this.labelOffsetWidth.Text = "Смещение по ширине";
+            // 
+            // labelHeight
+            // 
+            this.labelHeight.AutoSize = true;
+            this.labelHeight.Location = new System.Drawing.Point(126, 167);
+            this.labelHeight.Name = "labelHeight";
+            this.labelHeight.Size = new System.Drawing.Size(45, 13);
+            this.labelHeight.TabIndex = 14;
+            this.labelHeight.Text = "Высота";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(177, 164);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(65, 20);
+            this.textBox1.TabIndex = 15;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.textBox1);
+            this.groupBox1.Controls.Add(this.labelHeight);
+            this.groupBox1.Controls.Add(this.labelOffsetWidth);
+            this.groupBox1.Controls.Add(this.labelOffsetLong);
+            this.groupBox1.Controls.Add(this.labelWidhtUpperBase);
+            this.groupBox1.Controls.Add(this.labelLongUpperBase);
+            this.groupBox1.Controls.Add(this.labelWidhtBottomBase);
+            this.groupBox1.Controls.Add(this.labelLongBottomBase);
+            this.groupBox1.Controls.Add(this.textBoxOffsetWidth);
+            this.groupBox1.Controls.Add(this.textBoxOffsetLong);
+            this.groupBox1.Controls.Add(this.textBoxWidhtUpperBase);
+            this.groupBox1.Controls.Add(this.textBoxLongUpperBase);
+            this.groupBox1.Controls.Add(this.textBoxWidhtBottomBase);
+            this.groupBox1.Controls.Add(this.textBoxLongBottomBase);
+            this.groupBox1.Controls.Add(this.button2);
+            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Location = new System.Drawing.Point(419, 31);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(511, 213);
+            this.groupBox1.TabIndex = 16;
+            this.groupBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(936, 359);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBoxLongBottomBase;
+        private System.Windows.Forms.TextBox textBoxWidhtBottomBase;
+        private System.Windows.Forms.TextBox textBoxLongUpperBase;
+        private System.Windows.Forms.TextBox textBoxWidhtUpperBase;
+        private System.Windows.Forms.TextBox textBoxOffsetLong;
+        private System.Windows.Forms.TextBox textBoxOffsetWidth;
+        private System.Windows.Forms.Label labelLongBottomBase;
+        private System.Windows.Forms.Label labelWidhtBottomBase;
+        private System.Windows.Forms.Label labelLongUpperBase;
+        private System.Windows.Forms.Label labelWidhtUpperBase;
+        private System.Windows.Forms.Label labelOffsetLong;
+        private System.Windows.Forms.Label labelOffsetWidth;
+        private System.Windows.Forms.Label labelHeight;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.GroupBox groupBox1;
+    }
+}
+
