767cf8ec0d6666a31eb4e541efbeed05df4ec253
Michail Dyurilen <mr.dyurilen@mail.ru>
2016-02-19 15:30:24 +0300

    #08 Paint pyramid 2D  view from above.

--- WindowsFormsApplication1/Form1.cs
+++ WindowsFormsApplication1/Form1.cs
@@ -37,16 +37,16 @@ namespace WindowsFormsApplication1
 
         private void MakeTextBox(Model obj)// Convert.ToString выводим на фарму 
         {
-            textBoxAB.Text =Convert.ToString(LongBottomBase);//блок вывода нижнего основания 
+            textBoxAB.Text = Convert.ToString(LongBottomBase);//блок вывода нижнего основания 
             textBoxBC.Text = Convert.ToString(WidthBottomBase);
             textBoxCD.Text = Convert.ToString(LongBottomBase);
             textBoxDA.Text = Convert.ToString(WidthBottomBase);
-            
+
             textBoxA1A.Text = Convert.ToString(obj.SouthWest);//блок вывода левого! ребра
             textBoxB1B.Text = Convert.ToString(obj.SouthEast);
             textBoxC1C.Text = Convert.ToString(obj.NorthEast);
             textBoxD1D.Text = Convert.ToString(obj.NorthWest);
-            
+
             textBoxA1B1.Text = Convert.ToString(LongUpperBase);// блок  вывода верхнего основания
             textBoxB1C1.Text = Convert.ToString(WidthUpperBase);
             textBoxC1D1.Text = Convert.ToString(LongUpperBase);
@@ -73,18 +73,18 @@ namespace WindowsFormsApplication1
             DrawRectangle();
 
         }
-        private void DrawRectangle()
+
+        private void DrawRectangle()//рисуем на форме пирамиду, вид сверху
         {
-            int halfPoint11=12;
-            int halfPoint12=12;
-            int halfPoint21=390;
-            int halfPoint22=251;
+            int halfPoint11 = 12;
+            int halfPoint12 = 12;
+            int halfPoint21 = 390;
+            int halfPoint22 = 251;
 
-            int halfPoint31=150;
-            int halfPoint32=90;
-            int halfPoint41=100;
-            int halfPoint42 =50;
-
+            int halfPoint31 = 150;
+            int halfPoint32 = 130;
+            int halfPoint41 = 100;
+            int halfPoint42 = 50;
 
             System.Drawing.Pen myPen = new System.Drawing.Pen(System.Drawing.Color.Black);
             System.Drawing.Graphics formGraphics;
@@ -93,23 +93,13 @@ namespace WindowsFormsApplication1
             formGraphics.DrawRectangle(myPen, new Rectangle(halfPoint11, halfPoint12, halfPoint21, halfPoint22));//прямоугольник
             formGraphics.DrawRectangle(myPen, new Rectangle(halfPoint31, halfPoint32, halfPoint41, halfPoint42));//
             formGraphics.DrawLine(myPen, halfPoint11, halfPoint12, halfPoint31, halfPoint32); //линия
-            //formGraphics.DrawLine(myPen, halfPoint41, halfPoint32, halfPoint12, halfPoint21); //линия
-            //formGraphics.DrawLine(myPen, 12, 12, 60, 60); //линия
-            //formGraphics.DrawLine(myPen, halfPoint21, halfPoint22, halfPoint41, halfPoint42); //линия
-
-            //Point[] myPointArray = { new Point(0, 0), new Point(50, 30), new Point(30, 60) };//треугольник
-            //formGraphics.DrawPolygon(myPen, myPointArray);
-
+            formGraphics.DrawLine(myPen, halfPoint11 + halfPoint21, halfPoint12, halfPoint31 + halfPoint41, halfPoint32);
+            formGraphics.DrawLine(myPen, halfPoint31, halfPoint32 + halfPoint42, halfPoint11, halfPoint12 + halfPoint22);
+            formGraphics.DrawLine(myPen, halfPoint31 + halfPoint41, halfPoint32 + halfPoint42, halfPoint11 + halfPoint21, halfPoint12 + halfPoint22);
             myPen.Dispose();
             formGraphics.Dispose();
+            //Point[] myPointArray = { new Point(0, 0), new Point(50, 30), new Point(30, 60) };//треугольник
+            //formGraphics.DrawPolygon(myPen, myPointArray);
         }
-        //private void prLine()
-        //{
-        //    Pen pen = new Pen(Color.FromArgb(255, 0, 0, 0));
-        //    e.Graphics.DrawLine(pen, 20, 10, 300, 100);
-
-        //    Point[] myPointArray = { new Point(0, 0), new Point(50, 30), new Point(30, 60) };
-        //    myGraphics.DrawPolygon(myPen, myPointArray);
-        //}
     }
 }
